59f2c6cb9d11f233b482f02367304d493b740460
Guillermo Santos <guillermo.santosba@gmail.com>
2023-06-22 15:32:50 -0600

    Implemented movie details mapper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
@@ -37,65 +38,80 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final initialLoading = ref.watch(initialLoadingProvider);
-    if (initialLoading) {
-      return const FullScreenLoader();
-    }
+
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final upcomingMovies = ref.watch(upcomingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
+    return Stack(
+      children: [
+        Visibility(
+          visible: !initialLoading,
+          child: CustomScrollView(
+            slivers: [
+              const SliverAppBar(
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: CustomAppbar(),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+                  return Column(
+                    children: [
+                      MoviesSlideshow(movies: slideShowMovies),
+                      MovieHorizontalListView(
+                        movies: nowPlayingMovies,
+                        title: 'En cines',
+                        subtitle: 'Lunes 20',
+                        loadNextPage: () {
+                          ref
+                              .read(nowPlayingMoviesProvider.notifier)
+                              .loadNextPage();
+                        },
+                      ),
+                      MovieHorizontalListView(
+                        movies: upcomingMovies,
+                        title: 'Próximamente',
+                        subtitle: 'En este mes',
+                        loadNextPage: () {
+                          ref
+                              .read(upcomingMoviesProvider.notifier)
+                              .loadNextPage();
+                        },
+                      ),
+                      MovieHorizontalListView(
+                        movies: popularMovies,
+                        title: 'Populares',
+                        loadNextPage: () {
+                          ref
+                              .read(popularMoviesProvider.notifier)
+                              .loadNextPage();
+                        },
+                      ),
+                      MovieHorizontalListView(
+                        movies: topRatedMovies,
+                        title: 'Mejor calificadas',
+                        subtitle: 'Desde Siempre',
+                        loadNextPage: () {
+                          ref
+                              .read(topRatedMoviesProvider.notifier)
+                              .loadNextPage();
+                        },
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      )
+                    ],
+                  );
+                }, childCount: 1),
+              )
+            ],
           ),
         ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-            return Column(
-              children: [
-                MoviesSlideshow(movies: slideShowMovies),
-                MovieHorizontalListView(
-                  movies: nowPlayingMovies,
-                  title: 'En cines',
-                  subtitle: 'Lunes 20',
-                  loadNextPage: () {
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                MovieHorizontalListView(
-                  movies: upcomingMovies,
-                  title: 'Próximamente',
-                  subtitle: 'En este mes',
-                  loadNextPage: () {
-                    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                MovieHorizontalListView(
-                  movies: popularMovies,
-                  title: 'Populares',
-                  loadNextPage: () {
-                    ref.read(popularMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                MovieHorizontalListView(
-                  movies: topRatedMovies,
-                  title: 'Mejor calificadas',
-                  subtitle: 'Desde Siempre',
-                  loadNextPage: () {
-                    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                const SizedBox(
-                  height: 50,
-                )
-              ],
-            );
-          }, childCount: 1),
-        )
+        if (initialLoading)
+          FadeIn(child: Bounce(child: const FullScreenLoader()))
       ],
     );
   }
--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListView extends StatefulWidget {
   final List<Movie> movies;
@@ -100,7 +101,12 @@ class _Slide extends StatelessWidget {
                       strokeWidth: 2,
                     ));
                   }
-                  return FadeIn(child: child);
+                  return GestureDetector(
+                    child: FadeIn(child: child),
+                    onTap: () {
+                      context.push('/movie/${movie.id}');
+                    },
+                  );
                 },
               ),
             ),
